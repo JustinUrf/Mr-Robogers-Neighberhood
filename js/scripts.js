c6cfaff8378a76b9fbbdbb65e907190d86394c6e
Justin Lee <lee.justin001126@gmail.com>
2023-01-20 15:10:57 -0800

    add js logic that gives users feedback by turning text red when they input an invalid number

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Business Logic
 function myNeighbors(nums){
   let numsArray = []
-  for (i=0; i <= nums; i++){
+  for (i=0; i <= nums; i++) {
     if (i.toString().indexOf("3") > -1) {
       numsArray.push("Won't you be my neighbor?");
     }
@@ -15,20 +15,9 @@ function myNeighbors(nums){
     numsArray.push(i)
     }
   }
-  // for (i=0; i <= numsArray.length -1; i++){
-  //   if (numsArray[i].toString().indexOf("3") > -1) {
-  //     numsArray[i] = "Won't you be my neighbor?"
-  //   }
-  //   else if (numsArray[i].toString().indexOf("2") > - 1) {
-  //     numsArray[i] = "Boop!"
-  //   }
-  //   else if (numsArray[i].toString().indexOf("1") > - 1) {
-  //     numsArray[i] = "Beep!"
-  //   };
-  // };
   const theNewNeighborhood = numsArray.join(" ");
   return theNewNeighborhood;
-}
+};
 
 //UI logic
 function handleFormSubmission() {
@@ -36,12 +25,14 @@ function handleFormSubmission() {
   const userNumber = document.getElementById("usernumber-input-id").value;
   const theNeighborhood = myNeighbors(userNumber);
   if (Number.isInteger(parseInt(userNumber))) {
+    document.querySelector("div#user-feedback").removeAttribute("class", "error");
     document.getElementById("user-house-number").innerText = userNumber;
     document.getElementById("final-neighborhood").innerText = theNeighborhood;
   }
   else {
     document.getElementById("user-house-number").innerText = "Error";
     document.getElementById("final-neighborhood").innerText = "Please Enter a valid Number";
+    document.querySelector("div#user-feedback").setAttribute("class", "error");
   }
 
 }
