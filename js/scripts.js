f1f524dc47661c07522eaf6d078a955f9f46d76b
Justin Lee <lee.justin001126@gmail.com>
2023-01-20 14:08:22 -0800

    add js script that makes the array in one loop instead of 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,18 @@
 function myNeighbors(nums){
   let numsArray = []
   for (i=0; i <= nums; i++){
-    if (i.toString().index("3") > -1) {
-      numsArray.push("Won't you be my neighbor");
+    if (i.toString().indexOf("3") > -1) {
+      numsArray.push("Won't you be my neighbor?");
     }
+    else if (i.toString().indexOf("2") > -1) {
+      numsArray.push("Boop!");
+    }
+    else if (i.toString().indexOf("1") > -1) {
+      numsArray.push("Beep!");
+    }
+    else {
     numsArray.push(i)
+    }
   }
   // for (i=0; i <= numsArray.length -1; i++){
   //   if (numsArray[i].toString().indexOf("3") > -1) {
@@ -18,8 +26,8 @@ function myNeighbors(nums){
   //     numsArray[i] = "Beep!"
   //   };
   // };
-  // const theNewNeighborhood = numsArray.join(" ");
-  // return theNewNeighborhood;
+  const theNewNeighborhood = numsArray.join(" ");
+  return theNewNeighborhood;
 }
 
 //UI logic
