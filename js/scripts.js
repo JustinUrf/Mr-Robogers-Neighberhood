5a73ab7aea3522a11411f2052f2c15276832e6b0
Justin Lee <lee.justin001126@gmail.com>
2023-01-20 13:42:47 -0800

    add js logic to make the array in O(n) time instead of 2*O(n) time wip.

--- js/scripts.js
+++ js/scripts.js
@@ -2,34 +2,32 @@
 function myNeighbors(nums){
   let numsArray = []
   for (i=0; i <= nums; i++){
+    if (i.toString().index("3") > -1) {
+      numsArray.push("Won't you be my neighbor");
+    }
     numsArray.push(i)
   }
-  for (i=0; i <= numsArray.length -1; i++){
-    if (numsArray[i].toString().indexOf("3") > -1) {
-      numsArray[i] = "Won't you be my neighbor?"
-    }
-    else if (numsArray[i].toString().indexOf("2") > - 1) {
-      numsArray[i] = "Boop!"
-    }
-    else if (numsArray[i].toString().indexOf("1") > - 1) {
-      numsArray[i] = "Beep!"
-    };
-  };
-  const theNewNeighborhood = numsArray.join(" ");
-  return theNewNeighborhood;
+  // for (i=0; i <= numsArray.length -1; i++){
+  //   if (numsArray[i].toString().indexOf("3") > -1) {
+  //     numsArray[i] = "Won't you be my neighbor?"
+  //   }
+  //   else if (numsArray[i].toString().indexOf("2") > - 1) {
+  //     numsArray[i] = "Boop!"
+  //   }
+  //   else if (numsArray[i].toString().indexOf("1") > - 1) {
+  //     numsArray[i] = "Beep!"
+  //   };
+  // };
+  // const theNewNeighborhood = numsArray.join(" ");
+  // return theNewNeighborhood;
 }
-
-
-
-
-
 
 //UI logic
 function handleFormSubmission() {
   event.preventDefault();
   const userNumber = document.getElementById("usernumber-input-id").value;
   const theNeighborhood = myNeighbors(userNumber);
-  if (Number.isInteger(userNumber)) {
+  if (Number.isInteger(parseInt(userNumber))) {
     document.getElementById("user-house-number").innerText = userNumber;
     document.getElementById("final-neighborhood").innerText = theNeighborhood;
   }
