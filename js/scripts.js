b0638b2c7a1c3a1fb56670f352a289a29e54d477
Justin Lee <lee.justin001126@gmail.com>
2023-01-20 10:09:30 -0800

    add js function that makes an array based off user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 //Business Logic
 function myNeighbors(nums){
-
+  let numsArray = []
+  for (i=0; i <= nums; i++){
+    numsArray.push(i)
+  }
+  console.log(numsArray)
 }
 
 
@@ -10,7 +14,13 @@ function myNeighbors(nums){
 //UI logic
 function handleFormSubmission() {
   event.preventDefault();
-  const userNumber = document.getElementById("usernumber-input-id").value.
+  const userNumber = document.getElementById("usernumber-input-id").value;
   const theNeighborhood = myNeighbors(userNumber);
   document.getElementById("user-house-number").innerText = userNumber;
+  document.getElementById("final-neighborhood").innerText = theNeighborhood;
 }
+
+// Event Listener
+window.addEventListener("load", function(){
+  this.document.querySelector("form#robogers-neighborhood").addEventListener("submit", handleFormSubmission);
+});
